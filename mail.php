2d7cdef78de4a64642bd8ff548496f46e6819d44
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-08-28 12:25:51 +0900

    Done
    Done

---
+++ mail.php
@@ -0,0 +1,15 @@
+<?php
+
+	
+//the subject
+$sub = "Your subject";
+//the message
+$msg = "Your message TEST the email";
+//recipient email here
+$rec = "jm.kagaya25@gmail.com";
+//send email
+
+mail($rec,$sub,$msg);
+
+ 
+?>
